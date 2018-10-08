c9857828b57c057956797b646fce781cde441929
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-08 13:40:18 +0200

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ViewerPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'viewer', component: ViewerPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 import { AppComponent } from './app.component';
 import { ViewerPageComponent } from './components/pages/viewer-page/viewer-page.component';
@@ -19,8 +28,10 @@ import { PageNotFoundComponent } from './components/pages/page-not-found/page-no
   imports: [
     BrowserModule,
     NgbModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
