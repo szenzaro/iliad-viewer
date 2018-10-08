49fee98701f3fac94e13a62dd927c25d1e9b685f
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-08 13:38:12 +0200

    Add Viewer page stub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ViewerPageComponent } from './components/pages/viewer-page/viewer-page.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ViewerPageComponent,
     HomePageComponent,
   ],
   imports: [
