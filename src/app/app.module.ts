76e5e801c2d0e4de17b5c0c8529fe338cfce2378
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-08 13:39:13 +0200

    Add About page stub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ViewerPageComponent } from './components/pages/viewer-page/viewer-page.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewerPageComponent,
     HomePageComponent,
+    AboutPageComponent,
   ],
   imports: [
     BrowserModule,
