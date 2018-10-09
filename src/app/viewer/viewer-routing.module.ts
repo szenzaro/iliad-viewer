988dc61d8fed619f5d2e54789b2411013a5edce3
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-09 11:56:19 +0200

    Add viewer child routing

--- src/app/viewer/viewer-routing.module.ts
+++ src/app/viewer/viewer-routing.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ViewerPageComponent } from './viewer-page/viewer-page.component';
+import { ManuscriptComponent } from './components/manuscript/manuscript.component';
+import { TextComparisonComponent } from './components/text-comparison/text-comparison.component';
 
 const routes: Routes = [
-  { path: 'viewer', component: ViewerPageComponent },
+  {
+    path: 'viewer', component: ViewerPageComponent,
+    children: [
+      {
+        path: 'manuscript',
+        component: ManuscriptComponent,
+      },
+      {
+        path: 'texts',
+        component: TextComparisonComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'manuscript',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 
 @NgModule({
