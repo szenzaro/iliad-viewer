4d04723cf27225d1618e06684410eea91ef9f3f2
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-12 18:31:35 +0200

    Change getVerses to return Observable<Verses[]>

--- src/app/services/text.service.ts
+++ src/app/services/text.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Verse, Word, Chant } from '../utils/models';
+import { Observable } from 'rxjs';
+
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +12,15 @@ export class TextService {
 
   constructor(private readonly http: HttpClient) { }
 
-  getVerses(text: string, chant: number) {
-    return this.http.get(`./assets/texts/${text}/${chant}/verses.json`);
+  getVerses(text: string, chant: number): Observable<Verse[]> {
+    return this.http.get(`./assets/texts/${text}/${chant}/verses.json`)
+      .pipe(
+        tap((x: Chant) => console.log(x)),
+        map(({ verses }: Chant) => verses
+          .map((verse, i) => ({
+            n: 1,
+            words: verse.map((lemma, j) => ({ id: `${chant}.${i + 1}.${j + 1}`, lemma } as Word)),
+          } as Verse))),
+      );
   }
 }
