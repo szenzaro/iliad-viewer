54b022f3e0009107f0453741cd79420998169ad2
Simone Zenzaro <s.zenzaro@gmail.com>
2018-10-18 12:18:34 +0200

    Use openseadragon component in viewer-page

--- src/app/viewer/viewer-page/viewer-page.component.ts
+++ src/app/viewer/viewer-page/viewer-page.component.ts
@@ -9,6 +9,27 @@ import { Observable, NEVER } from 'rxjs';
 })
 export class ViewerPageComponent {
 
+  viewerOptions = {
+    preserveViewport: true,
+    visibilityRatio: 1,
+    minZoomLevel: 1,
+    defaultZoomLevel: 1,
+    sequenceMode: true,
+    tileSources: [{
+      '@context': 'http://iiif.io/api/image/2/context.json',
+      '@id': 'https://libimages1.princeton.edu/loris/pudl0001%2F4609321%2Fs42%2F00000001.jp2',
+      'height': 7200,
+      'width': 5233,
+      'profile': ['http://iiif.io/api/image/2/level2.json'],
+      'protocol': 'http://iiif.io/api/image',
+      'tiles': [{
+        'scaleFactors': [1, 2, 4, 8, 16, 32],
+        'width': 1024
+      }]
+    },
+    ]
+  };
+
   data: Observable<Object> = NEVER;
 
   constructor(private readonly textService: TextService) {
