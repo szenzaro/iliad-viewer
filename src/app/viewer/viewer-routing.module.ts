2e82144a0f570ac9e77cf66d3a03e104c0232d79
Simone Zenzaro <s.zenzaro@gmail.com>
2018-11-07 11:22:10 +0100

    Add search route

--- src/app/viewer/viewer-routing.module.ts
+++ src/app/viewer/viewer-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ManuscriptComponent } from './components/manuscript/manuscript.component';
+import { SearchComponent } from './components/search/search.component';
 import { TextComparisonComponent } from './components/text-comparison/text-comparison.component';
 import { ViewerPageComponent } from './viewer-page/viewer-page.component';
 
@@ -15,6 +17,10 @@ const routes: Routes = [
       {
         path: 'texts',
         component: TextComparisonComponent,
+      },
+      {
+        path: 'search',
+        component: SearchComponent,
       },
       {
         path: '',
