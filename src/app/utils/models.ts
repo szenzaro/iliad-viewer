fead4670e64d80a00a963ddcc77629f9a49df1cb
Simone Zenzaro <s.zenzaro@gmail.com>
2018-12-14 12:28:25 +0100

    Update models to manage omisit, tit, fin

--- src/app/utils/models.ts
+++ src/app/utils/models.ts
@@ -5,12 +5,23 @@ export interface Word {
 }
 
 export interface Verse {
+    id: number;
     n: number;
     words: Word[];
 }
 
+export type VerseKind = 'o' | 't' | 'v' | 'f';
+
+export type VerseType = ['v', number, string[]];
+export type OmisitTypeType = ['o', number];
+export type FinType = ['f', string[]];
+export type TitleType = ['t', string[]];
+
+export type VerseRowType = VerseType | OmisitTypeType | FinType | TitleType;
+
+
 export interface Chant {
     title: string;
     n: number;
-    verses: string[][];
+    verses: VerseRowType[];
 }
