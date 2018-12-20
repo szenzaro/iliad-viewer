fb2708271000b2a0136112acf564df879983f0bc
Simone Zenzaro <s.zenzaro@gmail.com>
2018-12-20 11:33:30 +0100

    Use comparable-text in text-comparison component

--- src/app/viewer/components/text-comparison/text-comparison.component.ts
+++ src/app/viewer/components/text-comparison/text-comparison.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { TextService } from 'src/app/services/text.service';
-import { Verse } from 'src/app/utils/models';
-
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-text-comparison',
@@ -11,27 +6,4 @@ import { filter, switchMap } from 'rxjs/operators';
   styleUrls: ['./text-comparison.component.scss'],
 })
 export class TextComparisonComponent {
-
-  textChanges = new BehaviorSubject<string>(undefined);
-  chantChanged = new BehaviorSubject<number>(1);
-
-  comparisonTextChanges = new BehaviorSubject<string>(undefined);
-  comparisonChantChanged = new BehaviorSubject<number>(1);
-
-  text: Observable<Verse[]> = combineLatest(this.textChanges, this.chantChanged)
-    .pipe(
-      filter(([text]) => !!text),
-      switchMap(([text, chant]) => this.textService.getVerses(text, chant)),
-    );
-
-  comparisonText: Observable<Verse[]> = combineLatest(this.comparisonTextChanges, this.comparisonChantChanged)
-    .pipe(
-      filter(([text]) => !!text),
-      switchMap(([text, chant]) => this.textService.getVerses(text, chant)),
-    );
-
-
-  constructor(private readonly textService: TextService) {
-  }
-
 }
