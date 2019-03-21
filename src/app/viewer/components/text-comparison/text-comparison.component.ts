5c1ef80a3888f3dc35d067b8f92593be95f1fb02
Simone Zenzaro <s.zenzaro@gmail.com>
2019-03-21 11:08:04 +0100

    Sync filters between comparable-texts

--- src/app/viewer/components/text-comparison/text-comparison.component.ts
+++ src/app/viewer/components/text-comparison/text-comparison.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PosFilter } from 'src/app/utils';
 
 @Component({
   selector: 'app-text-comparison',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-comparison.component.scss'],
 })
 export class TextComparisonComponent {
+  filter: PosFilter;
 }
