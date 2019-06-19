76451c37809e4a550f1c98de8cb31d55e3a682ae
Simone Zenzaro <s.zenzaro@gmail.com>
2019-06-19 10:55:11 +0200

    Remove mode-selector component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+interface OptionItem {
+  label: string;
+  path: string;
+  active: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iliad-viewer';
+
+  defaultOptions: OptionItem[] = [
+    { active: false, label: 'Project Description', path: '/home' },
+    { active: true, label: 'Manuscript', path: 'viewer/manuscript' },
+    { active: false, label: 'Text Comparison', path: 'viewer/texts' },
+    { active: false, label: 'Search/Analytics', path: 'viewer/search' },
+    { active: false, label: 'About', path: '/about' },
+  ];
 }
