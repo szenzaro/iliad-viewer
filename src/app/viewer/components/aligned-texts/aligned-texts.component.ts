b72377fd889b3bc74e315a42e5578e09bac405ff
Simone Zenzaro <s.zenzaro@gmail.com>
2019-06-25 10:27:00 +0200

    Show alignments via highlightIds

--- src/app/viewer/components/aligned-texts/aligned-texts.component.ts
+++ src/app/viewer/components/aligned-texts/aligned-texts.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter } from '@angular/core';
+import { combineLatest, merge } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 import { TextService } from 'src/app/services/text.service';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-aligned-texts',
@@ -8,11 +9,33 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./aligned-texts.component.scss']
 })
 export class AlignedTextsComponent {
-  readonly wordOver = new EventEmitter<string>();
+  readonly leftWordOver = new EventEmitter<string>();
+  readonly rightWordOver = new EventEmitter<string>();
 
-  al1 = this.wordOver.pipe(
+  al1 = this.leftWordOver.pipe(
     switchMap((x) => this.textService.getAlignment('homeric', 'paraphrase', x)),
   );
+  al2 = this.rightWordOver.pipe(
+    switchMap((x) => this.textService.getAlignment('paraphrase', 'homeric', x)),
+  );
+
+  sourceIds = merge(
+    this.al1.pipe(map((x) => !!x ? x.source : [])),
+    this.al2.pipe(map((x) => !!x ? x.target : [])),
+  );
+
+  targetIds = merge(
+    this.al1.pipe(map((x) => !!x ? x.target : [])),
+    this.al2.pipe(map((x) => !!x ? x.source : [])),
+  );
+
+  highlights = combineLatest([
+    this.sourceIds,
+    this.targetIds,
+  ]).pipe(
+    map(([source, target]) => ({ source, target })),
+  );
+
 
   constructor(
     readonly textService: TextService,
