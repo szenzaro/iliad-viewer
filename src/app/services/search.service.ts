787af6bacb49575144189b591bffb886258d448c
Simone Zenzaro <s.zenzaro@gmail.com>
2019-09-03 15:05:19 +0200

    Update search process
    At the moment it only works with the homeric text
    
    TODO: extend to multiple texts search

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,24 +1,52 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { combineLatest, Observable, of } from 'rxjs';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { Map } from '../utils/index';
+import { Word } from '../utils/models';
+import { TextService } from './text.service';
 
-export interface SearchResult {
-  query: string;
-  result: string; // TODO: this should not be a string but a structured result
-}
+export type Index = Map<number[]>;
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  results = new BehaviorSubject<SearchResult[]>([]);
+  cache: Map<any> = {};
+  queryString = new BehaviorSubject<string>(undefined);
 
-  search(query: string) {
-    const resultsNumber = Math.floor(Math.random() * 9) + 1;
+  private words = this.textService.getWords('homeric');
 
-    this.results.next(new Array(resultsNumber).fill({ query, result: 'Test Result' }));
-    // TODO: replace mock result with real results
+  resultArray = this.queryString.pipe(
+    filter((x) => !!x),
+    switchMap((query) => this.cachedGet<Observable<Index>>('./assets/data/texts/homeric/index.json')
+      .pipe(
+        map((x) => {
+          const keys = Object.keys(x);
+          const re = new RegExp(`.*${query}.*`, 'g');
+          return keys.filter((r) => re.test(r)).map((k) => x[k]).reduce((r, v) => r.concat(v), []) as number[];
+        }),
+        map((res) => res),
+      )
+    ),
+  );
+
+  results = combineLatest([this.words, this.resultArray]).pipe(
+    map(([words, ids]) => ids.map((id) => words[id])),
+  );
+
+  constructor(
+    private http: HttpClient,
+    private textService: TextService,
+  ) {
   }
 
+  private cachedGet<T>(path: string): Observable<T> {
+    return !!this.cache[path]
+      ? of<T>(this.cache[path])
+      : this.http.get<T>(path).pipe(tap((x) => this.cache[path] = x));
+  }
 }
