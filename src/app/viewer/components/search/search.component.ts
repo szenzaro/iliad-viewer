e66a6a575905c63f92654b926bb67ebe02013459
Simone Zenzaro <s.zenzaro@gmail.com>
2019-09-03 15:07:29 +0200

    Update search component

--- src/app/viewer/components/search/search.component.ts
+++ src/app/viewer/components/search/search.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
+import { map } from 'rxjs/operators';
 import { SearchService } from 'src/app/services/search.service';
+import { groupBy, Map } from 'src/app/utils';
+import { Word } from 'src/app/utils/models';
 
 @Component({
   selector: 'app-search',
@@ -14,6 +15,15 @@ export class SearchComponent {
 
   faSearch = faSearch;
   searchTextbox = new FormControl();
+  results = this.searchService.results.pipe(
+    map((x) => groupBy(x, 'source')),
+    map((x) => {
+      const keys = Object.keys(x);
+      const m: Map<Map<Word[]>> = {};
+      keys.forEach((k) => m[k] = groupBy(x[k], 'chant'));
+      return m;
+    })
+  );
 
   constructor(
     public searchService: SearchService,
@@ -21,6 +31,6 @@ export class SearchComponent {
   }
 
   search() {
-    this.searchService.search(this.searchTextbox.value);
+    this.searchService.queryString.next(this.searchTextbox.value);
   }
 }
