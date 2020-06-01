9e2b159d2dd952d07945e5862568b8bd44b0b291
Simone Zenzaro <s.zenzaro@gmail.com>
2020-06-01 14:52:56 +0200

    Higlight alignment filter words

--- src/app/viewer/components/word/word.component.ts
+++ src/app/viewer/components/word/word.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { faMars, faNeuter, faVenus } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { filter, map, shareReplay, switchMap } from 'rxjs/operators';
+import { AlignmentKind, AlignmentService } from 'src/app/services/alignment.service';
 import {
   containsPOStoHighlight,
   is1st,
@@ -47,9 +50,10 @@ import {
   isToponym,
   isVerb,
   isVocative,
-  PosFilter,
+  WordsFilter,
 } from 'src/app/utils';
 import { Word } from 'src/app/utils/models';
+import { InSubject } from '../../utils/in-subject';
 
 @Component({
   selector: 'app-word',
@@ -58,177 +62,212 @@ import { Word } from 'src/app/utils/models';
 })
 export class WordComponent {
 
-  @Input() posHighlight: PosFilter;
+  @InSubject() @Input() posHighlight: WordsFilter;
+  posHighlightChange = new BehaviorSubject<WordsFilter>(undefined);
   @Input() highlighted = false;
-  @Input() word: Word;
+  @InSubject() @Input() word: Word;
+  wordChange = new BehaviorSubject<Word>(undefined);
+  alignmentHighlightChange = new BehaviorSubject<AlignmentKind[]>([]);
   @Output() openWordId = new EventEmitter<string>();
   @Output() openWord = new EventEmitter<Word>();
   @Output() wordOver = new EventEmitter<string>();
+
+  alignmentKind = combineLatest([
+    this.posHighlightChange.pipe(
+      filter((x) => !!x && !!x.alType),
+    ),
+    this.wordChange.pipe(
+      filter((x) => !!x),
+    ),
+  ]).pipe(
+    switchMap(([{ source, target, alType }, w]) => this.alignmentService.getWordAlignmentKind(w, source, target, alType)),
+    shareReplay(1),
+  );
+
+  alignmentHighlighted = combineLatest([
+    this.posHighlightChange.pipe(
+      filter((x) => !!x),
+    ),
+    this.alignmentKind.pipe(
+      filter((x) => !!x),
+    ),
+  ]).pipe(
+    map(([wfilter, k]) => wfilter.wfilter.includes(k)),
+    shareReplay(1),
+  );
 
   faNeuter = faNeuter;
   faMars = faMars;
   faVenus = faVenus;
 
+  constructor(
+    public alignmentService: AlignmentService,
+  ) {
+  }
+
   get posHighlighted() {
-    return !!this.posHighlight && !!this.posHighlight.pos && this.posHighlight.pos.length > 0 &&
+    return !!this.posHighlight && !!this.posHighlight.wfilter && this.posHighlight.wfilter.length > 0 &&
       !!this.word &&
       !!this.word.data &&
       !!this.word.data.tag &&
+      !this.posHighlight.source &&
       containsPOStoHighlight(this.word.data.tag, this.posHighlight);
   }
 
   get tagIsDefined() { return !!this.word && !!this.word.data && !!this.word.data.tag; }
 
   get posNeg() {
-    return this.tagIsDefined && isNeg(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Neg');
+    return this.tagIsDefined && isNeg(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Neg');
   }
 
   get posConj() {
-    return this.tagIsDefined && isConj(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Conj');
+    return this.tagIsDefined && isConj(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Conj');
   }
 
   get posIntj() {
-    return this.tagIsDefined && isIntj(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Intj');
+    return this.tagIsDefined && isIntj(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Intj');
   }
 
   get posAdjective() {
-    return this.tagIsDefined && isAdjective(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Adjective');
+    return this.tagIsDefined && isAdjective(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Adjective');
   }
   get posParticle() {
-    return this.tagIsDefined && isParticle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Particle');
+    return this.tagIsDefined && isParticle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Particle');
   }
   get posPreposition() {
-    return this.tagIsDefined && isPreposition(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Preposition');
+    // tslint:disable-next-line: max-line-length
+    return this.tagIsDefined && isPreposition(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Preposition');
   }
   get posArticle() {
-    return this.tagIsDefined && isArticle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Article');
+    return this.tagIsDefined && isArticle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Article');
   }
   get posAdverb() {
-    return this.tagIsDefined && isAdverb(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Adverb');
+    return this.tagIsDefined && isAdverb(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Adverb');
   }
   get posName() {
-    return this.tagIsDefined && isName(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Name');
+    return this.tagIsDefined && isName(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Name');
   }
   get posAnthroponymic() {
     return this.tagIsDefined && isAnthroponymic(this.word.data.tag) &&
-      !!this.posHighlight && this.posHighlight.pos.includes('Anthroponymic');
+      !!this.posHighlight && this.posHighlight.wfilter.includes('Anthroponymic');
   }
   get posVerb() {
-    return this.tagIsDefined && isVerb(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Verb');
+    return this.tagIsDefined && isVerb(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Verb');
   }
   get posPronoun() {
-    return this.tagIsDefined && isPronoun(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Pronoun');
+    return this.tagIsDefined && isPronoun(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Pronoun');
   }
   get posNum() {
-    return this.tagIsDefined && isNum(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Num');
+    return this.tagIsDefined && isNum(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Num');
   }
 
   get posMasculine() {
-    return this.tagIsDefined && isMasculine(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Masculine');
+    return this.tagIsDefined && isMasculine(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Masculine');
   }
 
   get posFeminine() {
-    return this.tagIsDefined && isFeminine(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Feminine');
+    return this.tagIsDefined && isFeminine(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Feminine');
   }
 
   get posNeutral() {
-    return this.tagIsDefined && isNeutral(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Neutral');
+    return this.tagIsDefined && isNeutral(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Neutral');
   }
 
   get posSingular() {
-    return this.tagIsDefined && isSingular(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Singular');
+    return this.tagIsDefined && isSingular(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Singular');
   }
 
   get posPlural() {
-    return this.tagIsDefined && isPlural(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Plural');
+    return this.tagIsDefined && isPlural(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Plural');
   }
 
   get posDual() {
-    return this.tagIsDefined && isDual(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Dual');
+    return this.tagIsDefined && isDual(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Dual');
   }
 
   get posNominative() {
-    return this.tagIsDefined && isNominative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Nominative');
+    return this.tagIsDefined && isNominative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Nominative');
   }
 
   get posVocative() {
-    return this.tagIsDefined && isVocative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Vocative');
+    return this.tagIsDefined && isVocative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Vocative');
   }
 
   get posAccusative() {
-    return this.tagIsDefined && isAccusative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Accusative');
+    return this.tagIsDefined && isAccusative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Accusative');
   }
 
   get posGenitive() {
-    return this.tagIsDefined && isGenitive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Genitive');
+    return this.tagIsDefined && isGenitive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Genitive');
   }
 
   get posDative() {
-    return this.tagIsDefined && isDative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Dative');
+    return this.tagIsDefined && isDative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Dative');
   }
 
   get posPresent() {
-    return this.tagIsDefined && isPresent(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Present');
+    return this.tagIsDefined && isPresent(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Present');
   }
   get posImperfect() {
-    return this.tagIsDefined && isImperfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Imperfect');
+    return this.tagIsDefined && isImperfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Imperfect');
   }
   get posFuture() {
-    return this.tagIsDefined && isFuture(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Future');
+    return this.tagIsDefined && isFuture(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Future');
   }
   get posAorist() {
-    return this.tagIsDefined && isAorist(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Aorist');
+    return this.tagIsDefined && isAorist(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Aorist');
   }
   get posPerfect() {
-    return this.tagIsDefined && isPerfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Perfect');
+    return this.tagIsDefined && isPerfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Perfect');
   }
   get posPluperfect() {
-    return this.tagIsDefined && isPluperfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Pluperfect');
+    return this.tagIsDefined && isPluperfect(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Pluperfect');
   }
   get posFuturePerfect() {
     return this.tagIsDefined && isFuturePerfect(this.word.data.tag) && !
-      !this.posHighlight && this.posHighlight.pos.includes('FuturePerfect');
+      !this.posHighlight && this.posHighlight.wfilter.includes('FuturePerfect');
   }
   get posIndicative() {
-    return this.tagIsDefined && isIndicative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Indicative');
+    return this.tagIsDefined && isIndicative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Indicative');
   }
   get posSubjunctive() {
-    return this.tagIsDefined && isSubjunctive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Subjunctive');
+    // tslint:disable-next-line: max-line-length
+    return this.tagIsDefined && isSubjunctive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Subjunctive');
   }
   get posImperative() {
-    return this.tagIsDefined && isImperative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Imperative');
+    return this.tagIsDefined && isImperative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Imperative');
   }
   get posOptative() {
-    return this.tagIsDefined && isOptative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Optative');
+    return this.tagIsDefined && isOptative(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Optative');
   }
   get posInfinitive() {
-    return this.tagIsDefined && isInfinitive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Infinitive');
+    return this.tagIsDefined && isInfinitive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Infinitive');
   }
   get posParticiple() {
-    return this.tagIsDefined && isParticiple(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Participle');
+    return this.tagIsDefined && isParticiple(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Participle');
   }
   get pos1st() {
-    return this.tagIsDefined && is1st(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('1st');
+    return this.tagIsDefined && is1st(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('1st');
   }
   get pos2nd() {
-    return this.tagIsDefined && is2nd(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('2nd');
+    return this.tagIsDefined && is2nd(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('2nd');
   }
   get pos3rd() {
-    return this.tagIsDefined && is3rd(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('3rd');
+    return this.tagIsDefined && is3rd(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('3rd');
   }
   get posToponym() {
-    return this.tagIsDefined && isToponym(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Toponym');
+    return this.tagIsDefined && isToponym(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Toponym');
   }
   get posPatronymic() {
-    return this.tagIsDefined && isPatronymic(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Patronymic');
+    return this.tagIsDefined && isPatronymic(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Patronymic');
   }
   get posActive() {
-    return this.tagIsDefined && isActive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Active');
+    return this.tagIsDefined && isActive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Active');
   }
   get posMiddle() {
-    return this.tagIsDefined && isMiddle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Middle');
+    return this.tagIsDefined && isMiddle(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Middle');
   }
   get posPassive() {
-    return this.tagIsDefined && isPassive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.pos.includes('Passive');
+    return this.tagIsDefined && isPassive(this.word.data.tag) && !!this.posHighlight && this.posHighlight.wfilter.includes('Passive');
   }
 }
