425b0b97f6231d265a1c07cace091e5abd23870c
Simone Zenzaro <s.zenzaro@gmail.com>
2020-08-21 19:10:35 +0200

    Add Scholie alignment support

--- src/app/services/alignment.service.ts
+++ src/app/services/alignment.service.ts
@@ -8,13 +8,20 @@ import { CacheService } from './cache.service';
 import { TextService } from './text.service';
 
 interface AlignmentEntry {
-  type: 'sub' | 'ins' | 'del' | 'eq';
+  type: AlignmentKind | ScholieKind;
+  source: string[];
+  target: string[];
+}
+
+interface ScholieEntry {
+  kind: 'h' | 'p';
   source: string[];
   target: string[];
 }
 
 export type AlignmentType = 'auto' | 'manual';
 export type AlignmentKind = 'sub' | 'ins' | 'del' | 'eq';
+export type ScholieKind = 'homerscholie' | 'homernotscholie' | 'paraphrasescholie';
 
 export const AlignmentLabels = {
   auto: _T('Auto'),
@@ -31,6 +38,11 @@ export class AlignmentService {
     shareReplay(1),
   );
 
+  private manifestScholieAlignmentsData = this.textService.manifest.pipe(
+    map(({ scholieAlignments }) => scholieAlignments),
+    shareReplay(1),
+  );
+
   private alignments = this.manifestAlignmentsData.pipe(
     map((al) => al.map(({ source, target, type }) => `./assets/data/alignments/${type}/${source}/${target}.json`)),
     map((al) => al.map((a) => this.cacheService.cachedGet<Map<AlignmentEntry>>(a))),
@@ -43,6 +55,35 @@ export class AlignmentService {
         return ret;
       })
     )),
+    shareReplay(1),
+  );
+
+  // TODO: refactor alignments and scholieAlignments as a single parametric function
+  private scholieAlignments = this.manifestScholieAlignmentsData.pipe(
+    map((al) => al.map(({ source, target }) => `./assets/data/scholie-alignment/${source}/${target}.json`)),
+    map((al) => al.map((a) => this.cacheService.cachedGet<Map<ScholieEntry>>(a))),
+    switchMap((al) => combineLatest([forkJoin(al), this.textService.manifest]).pipe(
+      map(([als, { scholieAlignments }]) => {
+        const ret: Map<Map<AlignmentEntry>> = {};
+        scholieAlignments.forEach((a, i) => {
+
+          const keys = Object.keys(als[i]);
+          const data: Map<AlignmentEntry> = {};
+          keys.forEach((k) => {
+            const { source, target, kind } = als[i][k];
+            const type = kind === 'h' ? 'homerscholie' : 'homernotscholie';
+            data[k] = { source, target, type };
+          });
+          ret[`scholie_${a.source}-${a.target}`] = data;
+        });
+        return ret;
+      })
+    )),
+    shareReplay(1),
+  );
+
+  homericScholieAlignmentsIDS = this.scholieAlignments.pipe( // TODO make this more robust
+    map((als) => als['scholie_homeric-scholie']),
     shareReplay(1),
   );
 
@@ -65,6 +106,20 @@ export class AlignmentService {
       );
   }
 
+  getScholieAlignment(source: string, target: string, wordId: string) {
+    return this.scholieAlignments
+      .pipe(
+        map((alignment) => alignment[`scholie_${source}-${target}`][wordId]), // Can be undefined!
+        map((al) => ({ ...al, source: al?.source ?? [], target: al?.target ?? [] } as AlignmentEntry)),
+      );
+  }
+
+  getScholieAlignmentChants(source: string, target: string) {
+    return this.manifestScholieAlignmentsData.pipe(
+      map((als) => als.find((v) => v.source === source && v.target === target)?.chants),
+    );
+  }
+
   getAlignmentChants(source: string, target: string, type: AlignmentType) {
     return this.manifestAlignmentsData.pipe(
       map((als) => als.find((v) => v.source === source && v.target === target && v.type === type)?.chants),
@@ -82,6 +137,20 @@ export class AlignmentService {
         target: w.source === source ? x.target : [...x.target, w.id],
       })),
       map((x) => x.type),
+    );
+  }
+
+  getWordScholieAlignmentKind(w: Word, source: string, target: string) {
+    return (w.source === source
+      ? this.getScholieAlignment(source, target, w.id)
+      : this.getScholieAlignment(target, source, w.id)
+    ).pipe(
+      map((x) => ({
+        ...x,
+        source: w.source === source ? [...(x.source), w.id] : w.source,
+        target: w.source === source ? x.target : [...x.target, w.id],
+      })),
+      map((x) => source === 'scholie' ? 'paraphrasescholie' : x.type),
     );
   }
 }
