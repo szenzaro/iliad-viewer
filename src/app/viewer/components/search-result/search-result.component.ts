4827bca59ee2f689ae6371dbde6162a839e187c7
Simone Zenzaro <s.zenzaro@gmail.com>
2020-09-22 17:31:43 +0200

    Add showChant to search result

--- src/app/viewer/components/search-result/search-result.component.ts
+++ src/app/viewer/components/search-result/search-result.component.ts
@@ -14,6 +14,7 @@ import { TextService } from 'src/app/services/text.service';
   styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResultComponent {
+  @Input() showChant = false;
   @Input() @InSubject() words: Word[];
   private wordsChange = new BehaviorSubject<Word[]>([]);
 
