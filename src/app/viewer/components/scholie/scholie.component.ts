bd798fdef50fa447e9266a389a60c66422c90432
Simone Zenzaro <s.zenzaro@gmail.com>
2020-09-30 13:50:28 +0200

    Add chant and verse filters

--- src/app/viewer/components/scholie/scholie.component.ts
+++ src/app/viewer/components/scholie/scholie.component.ts
@@ -1,13 +1,16 @@
 import { KeyValue } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { ScholieService } from 'src/app/services/scholie.service';
 
 import { marker as _T } from '@biesbjerg/ngx-translate-extract-marker';
 import { BehaviorSubject, combineLatest, merge, Subject } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
-import { AlignmentService } from 'src/app/services/alignment.service';
 import { numberToOption, WordsFilter } from 'src/app/utils';
 import { ScholieHelpComponent } from '../help/scholie-help/scholie-help.component';
+
+import { TranslateService } from '@ngx-translate/core';
+import { AlignmentService } from 'src/app/services/alignment.service';
+import { ScholieService } from 'src/app/services/scholie.service';
+import { TextService } from 'src/app/services/text.service';
 
 @Component({
   selector: 'app-scholie',
@@ -15,7 +18,39 @@ import { ScholieHelpComponent } from '../help/scholie-help/scholie-help.componen
   styleUrls: ['./scholie.component.css']
 })
 export class ScholieComponent {
-  scholie = this.scholieService.scholie;
+  private allOption = { id: 'all', label: this.ts.instant(_T('All')) };
+
+  chants = this.textService.getChants('homeric').pipe(
+    map((x) => x.map((n) => numberToOption(+n))),
+    map((x) => [this.allOption, ...x])
+  );
+  chant = new BehaviorSubject<number | 'all'>('all');
+  chantOption = this.chant.pipe(
+    map((c) => c === 'all' ? this.allOption : numberToOption(c)),
+  );
+
+  verses = this.chant.pipe(
+    switchMap((c) => c === 'all'
+      ? this.textService.getAllVerses('homeric')
+      : this.textService.getVerses('homeric', c)
+    ),
+    map((x) => x.filter(({ n }) => !isNaN(+n))),
+    map((x) => x.map(({ n }) => numberToOption(+n))),
+    map((x) => [this.allOption, ...x])
+  );
+
+  verse = new BehaviorSubject<number | 'all'>('all');
+  verseOption = this.verse.pipe(
+    map((v) => v === 'all' ? this.allOption : numberToOption(v)),
+  );
+
+  scholie = combineLatest([
+    this.chant,
+    this.verse,
+  ]).pipe(
+    map(([c, v]) => this.scholieService.filteredScholie(c, v)),
+  );
+
   ScholieHelpComponent = ScholieHelpComponent;
   filter = new Subject<WordsFilter>();
   filterData = [
@@ -89,6 +124,8 @@ export class ScholieComponent {
   constructor(
     private scholieService: ScholieService,
     private alignmentService: AlignmentService,
+    public textService: TextService,
+    private ts: TranslateService,
   ) {
   }
 }
