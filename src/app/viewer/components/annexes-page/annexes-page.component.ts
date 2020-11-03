b7fc86ea3835a6b79d888e0af24d20161e62d4ec
Simone Zenzaro <s.zenzaro@gmail.com>
2020-11-03 20:10:58 +0100

    Add annexes

--- src/app/viewer/components/annexes-page/annexes-page.component.ts
+++ src/app/viewer/components/annexes-page/annexes-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-annexes-page',
   templateUrl: './annexes-page.component.html',
   styleUrls: ['./annexes-page.component.scss']
 })
-export class AnnexesPageComponent implements OnInit {
+export class AnnexesPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    readonly translate: TranslateService,
+  ) {
   }
-
 }
